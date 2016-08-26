af1bcb6a359974f986eb96b24cb093007658fa29
ScorchSecurity <scorchsec@gmail.com>
2016-08-26 08:11:09 -0400

    Added some definitions that I had forgotten to include.

--- toast.h
+++ toast.h
@@ -5,6 +5,9 @@
 #include <winternl.h>
 #include <intrin.h>
 
+#define NtCurrentProcess() ( (HANDLE)(LONG_PTR) -1 )
+#define DEREF( pointer )*(ULONG_PTR *)(pointer)
+
 #ifdef __cplusplus
 extern "C" {
 #endif
